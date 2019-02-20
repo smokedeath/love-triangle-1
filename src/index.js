33472d51792f70a6739079a52bd335a45eff5b6e
smoke <smokedeath87@gmail.com>
2019-02-20 12:01:57 +0600

    test success

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+    var triangle = 0;
+    for (var i = 0; i < preferences.length; i++) {
+        var sp1 = i+1;
+        var sp2 = preferences[i];
+        var sp3 = preferences[sp2-1];
+        if (sp1 !== sp2 && sp2 !== sp3 && sp1 !== sp3)
+            if (sp1===preferences[sp3-1]) triangle++;
+        // console.log(sp1.toString()+' '+sp2.toString()+' '+sp3.toString()+' '+ triangle.toString());
+    }
+    return triangle > 0 ? triangle/3 : 0;
 };
